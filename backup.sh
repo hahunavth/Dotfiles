0c0b96dced8c7d1272860bd3643776b1bc2fced7
hahunavth <66118664+hahunavth@users.noreply.github.com>
2021-10-09 10:11:27 +0700

    Backup at Thứ bảy, 09 Tháng 10 năm 2021 10:11:27 +07

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git pull origin main
+git pull origin eos
 while read f; do
     echo "Backup file $f..."
     cp -rf $HOME/$f ./
@@ -9,4 +9,4 @@ echo "Backup files done, please commit!"
 
 git add . -A
 git commit -m "Backup at $(date)"
-git push origin main
+git push origin eos
