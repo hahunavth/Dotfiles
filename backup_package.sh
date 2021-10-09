32d8f9c069426ace6e46580f176e05eaf343a229
hahunavth <66118664+hahunavth@users.noreply.github.com>
2021-10-09 10:09:16 +0700

    Backup at Thứ bảy, 09 Tháng 10 năm 2021 10:09:16 +07

--- backup_package.sh
+++ backup_package.sh
@@ -1,7 +1,7 @@
 echo 'EXEC: rpm -qa > ./package_all.bak'
 rpm -qa > ./package_all.bak
 echo 'EXEC: sudo dnf history userinstalled > ./package_user.bak'
-sudo dnf history userinstalled > ./package_user.bak
+apt-mark showmanual > ./package_user.bak
 echo 'EXEC: snap list > package_snap.bak'
 snap list > package_snap.bak
 echo 'EXEC: flatpak list > package_flatpak.bak' 
