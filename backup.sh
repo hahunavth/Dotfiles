9c5d7a1318732e8649d77393fdc891acc2f323ed
hahunavth <66118664+hahunavth@users.noreply.github.com>
2021-10-09 15:07:15 +0700

    Backup at Thứ bảy, 09 Tháng 10 năm 2021 15:07:15 +07

--- backup.sh
+++ backup.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 git pull origin eos
+echo "========== FILES =========="
 while read f; do
-    echo "Backup file $f..."
+    echo "BACKUP FILE $f..."
     cp -rf $HOME/$f ./data/ 
 done < Dotfiles
+echo "========== COMMIT =========="
 echo "Backup files done, please commit!"
 
 git add . -A
 git commit -m "Backup at $(date)"
 git push origin eos
+
+echo "========== DONE =========="
--- backup_package.sh
+++ backup_package.sh
@@ -1,9 +1,9 @@
 echo 'EXEC: rpm -qa > ./package_all.bak'
-rpm -qa > ./package_all.bak
+rpm -qa > ./list/package_all.bak
 echo 'EXEC: sudo dnf history userinstalled > ./package_user.bak'
-apt-mark showmanual > ./package_user.bak
+apt-mark showmanual > ./list/package_user.bak
 echo 'EXEC: snap list > package_snap.bak'
-snap list > package_snap.bak
+snap list > ./list/package_snap.bak
 echo 'EXEC: flatpak list > package_flatpak.bak' 
-flatpak list > package_flatpak.bak
+flatpak list > ./list/package_flatpak.bak
 echo 'Enjoy!'
